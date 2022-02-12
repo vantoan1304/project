9e3f195da09b288f5308891659dcc219e8a27251
Van Toan <vantoan1304@gmail.com>
2022-02-12 15:03:16 +0700

    fix config

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define( 'DISALLOW_FILE_EDIT', true ); // Disable File Editor - Security > Settin
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'toan93_tomato' );
+define( 'DB_NAME', 'toan93_tomatoahi hi' );
 
 /** MySQL database username */
 define( 'DB_USER', 'toan93_tomato' );
